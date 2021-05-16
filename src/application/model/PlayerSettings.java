c5497110f743dc7dcb67054fdbcf29090820d6ca
vZimmeeeh <simoneaiello2000@outlook.com>
2021-05-16 19:15:03 +0200

    bug fix for gravity

--- src/application/model/PlayerSettings.java
+++ src/application/model/PlayerSettings.java
@@ -4,4 +4,5 @@ public class PlayerSettings {
 	public static final int MOVE_RIGHT = 0;
 	public static final int MOVE_LEFT = 1;
 	public static final int IDLE = 2;
+	public static final int DOWN = 3;
 }
